b01b875516b407cbaf10ef5a450387576e1ef303
Gururaj Gurram <gururajgurram1512@gmail.com>
2024-12-20 15:05:21 +0530

    add initial components and pages for banking application

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,35 @@
+import Headerbox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName: 'Gururaj', lastName: 'Gurram', email: 'gururajgurram1512@gmail.com'}
     return (
-        <div>
-            Home
-        </div>
+        <section className='home'>
+            <div className="home-content">
+                <header className="home-header">
+                    <Headerbox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?. firstName || 'Guest'}
+                        subtext="Access and manage your account and transactions efficiently"
+                    />
+
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1234.34}
+                    />
+                </header>
+                Recent Transactions
+            </div>
+            <RightSideBar
+                user={loggedIn}
+                transactions={[]}
+                banks={[{currentBalance: 3278.32}, {currentBalance: 876.32}]}
+            />
+        </section>
     )
 }
 
